a6a39bec219252968961a65a7fa9056a4ab086ac
TEJ PRATAP <malav2202@gmail.com>
2021-06-16 17:46:18 +0530

    17/06/21

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import Route from './scr/routes/Route'
-import Dashboard from './scr/screen/Dashboard';
-import News from './scr/screen/News';
-import ProductLayout from './scr/component/ProductLayout';
-import { shoe } from './scr/helper/helper';
 
 const App = () => {
   return (
     <>
-      {/* <News/>  */}
       <Route/>
     </>  
   )
